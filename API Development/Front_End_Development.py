d9915387305cfdaa2a8a48326276a7079f7ec7f6
aka-TheJew <101367388+aka-TheJew@users.noreply.github.com>
2022-09-20 05:41:02 +0300

    Add files via upload

---
+++ API Development/Front_End_Development.py
@@ -0,0 +1,72 @@
+import streamlit as st
+import base64
+
+# Functions
+
+
+def add_bg_from_local(image_file):
+    with open(image_file, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    st.markdown(
+        f"""
+    <style>
+    .stApp {{
+        background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
+        background-size: cover
+    }}
+    </style>
+    """,
+        unsafe_allow_html=True
+    )
+
+# Lists
+
+
+page_options = [
+    "Landing Page",
+    "Live Feed",
+    "Modelling",
+    "Explorative Data Analysis [EDA]",
+    "Train Simulation",
+    "Customer Experience"
+]
+api_options = [
+    "Air Quality",
+    "Line"
+]
+
+# Dictionaries
+
+
+api_dict = {
+    "accidentStats": "https://api.tfl.gov.uk/AccidentStats/{year}",
+    "AirQuality": "https://api.tfl.gov.uk/AirQuality/",
+    "BikePoint": "https://api.tfl.gov.uk/BikePoint/",
+    "Journey": "https://api.tfl.gov.uk/Journey",
+    "Line": "https://api.tfl.gov.uk/Line",
+    "Mode": "https://api.tfl.gov.uk/Mode/{mode}/Arrivals[?count]",
+    "Occupancy": "https://api.tfl.gov.uk/Occupancy",
+    "Road": "https://api.tfl.gov.uk/Road/{ids}/Disruption[?stripContent][&severities][&categories][&closures]",
+    "Search": "https://api.tfl.gov.uk/Search/Meta/Categories",
+    "Station": "https://api.tfl.gov.uk/stationdata/tfl-stationdata-detailed.zip",
+    "StopPoint": "https://api.tfl.gov.uk/StopPoint"
+}
+
+
+def main():
+    st.sidebar.image('Assets/LU_logo.jpg', use_column_width=True)
+    page_selection = st.sidebar.selectbox("PAGE SELECTION", page_options)
+
+    if page_selection == "Landing Page":
+        add_bg_from_local('Assets/Landing_Page_Background.jpg')
+
+    if page_selection == "Live Feed":
+        api_selection = st.selectbox("API SELECTION", api_options)
+        if api_selection == 'Air Quality':
+            st.subheader(api_dict['AirQuality'])
+        if api_selection == 'Line':
+            st.subheader(api_dict['Line'])
+
+
+if __name__ == '__main__':
+    main()
